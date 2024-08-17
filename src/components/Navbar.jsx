abf5f2db1742507dbdcf5044add2d8f5ae5f95bf
bharat <bharatdhiman40@gmail.com>
2024-08-17 13:06:00 +0530

    navbar icons changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,34 @@
+import { PiAirplaneTiltLight } from "react-icons/pi";
+import { LuHotel } from "react-icons/lu";
+import { IoTrainOutline } from "react-icons/io5";
+import { IoCarOutline } from "react-icons/io5";
+import { IoBusOutline } from "react-icons/io5";
 const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 w-full z-50">
       <ul className="flex justify-center gap-20 bg-gray-800 text-white items-center h-14">
-        <h1 className="text-2xl font-bold hover:text-orange-500 duration-200">
+        <h1 className="text-2xl font-bold">
           <span className="text-orange-500">U</span>daariyaan
         </h1>
-        <li className="hover:text-orange-500 cursor-pointer duration-200 text-slate-300">
-          ✈️Flights
+        <li className="hover:text-orange-500 cursor-pointer duration-200 text-slate-200 flex">
+          <PiAirplaneTiltLight className="text-2xl" />
+          Flights
         </li>
-        <li className="hover:text-orange-500 cursor-pointer duration-200 text-slate-300">
-          🏢Hotels
+        <li className="hover:text-orange-500 cursor-pointer duration-200 text-slate-200 flex">
+          <LuHotel className="text-2xl" />
+          Hotels
         </li>
-        <li className="hover:text-orange-500 cursor-pointer duration-200 text-slate-300">
-          🚅Trains
+        <li className="hover:text-orange-500 cursor-pointer duration-200 text-slate-200 flex">
+          <IoTrainOutline className="text-2xl" />
+          Trains
         </li>
-        <li className="hover:text-orange-500 cursor-pointer duration-200 text-slate-300">
-          🚕Cabs
+        <li className="hover:text-orange-500 cursor-pointer duration-200 text-slate-200 flex">
+          <IoCarOutline className="text-2xl" />
+          Cabs
         </li>
-        <li className="hover:text-orange-500 cursor-pointer duration-200 text-slate-300">
-          🚌Bus
+        <li className="hover:text-orange-500 cursor-pointer duration-200 text-slate-200 flex">
+          <IoBusOutline className="text-2xl" />
+          Bus
         </li>
         <li className="hover:text-indigo-500 cursor-pointer duration-200">
           Manage Booking
