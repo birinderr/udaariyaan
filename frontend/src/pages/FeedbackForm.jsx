1a5cf6eccfb1c8c5a5d4b8e0b16358e4381c1824
bharat <bharatdhiman40@gmail.com>
2024-11-21 10:10:56 +0530

    images added to feedback and error page

--- frontend/src/pages/FeedbackForm.jsx
+++ frontend/src/pages/FeedbackForm.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import FeedbackCard from "../components/FeedbackCard";
-import ScrollToTop from '../components/ScrollToTop.jsx';
+import ScrollToTop from "../components/ScrollToTop.jsx";
 
 const FeedbackForm = () => {
   const [name, setName] = useState("");
@@ -60,62 +60,74 @@ const FeedbackForm = () => {
   return (
     <section>
       <ScrollToTop />
-      <div className="flex">
-        <div className="w-[500px] mt-10 p-6 bg-gray-100  border shadow-[0_3px_10px_rgb(0,0,0,0.2)]s">
-          <h2 className="text-2xl font-bold mb-4 text-center">Feedback Form</h2>
-          <form className="space-y-4" onSubmit={handleSubmit}>
-            <div>
-              <label
-                htmlFor="name"
-                className="block text-sm font-medium text-gray-700 mb-1"
-              >
-                Name
-              </label>
-              <input
-                type="text"
-                id="name"
-                value={name}
-                onChange={(e) => {
-                  setName(e.target.value);
-                }}
-                name="name"
-                className="w-full px-3 py-2 border border-gray-300  focus:outline-none focus:ring focus:border-blue-300"
-                placeholder="Enter your name"
-                required
-              />
-            </div>
+      <div className="flex gap-14 items-center justify-center">
+        <div className="flex">
+          <div className="w-[500px] mt-10 p-6 bg-gray-100  border shadow-[0_3px_10px_rgb(0,0,0,0.2)]s">
+            <h2 className="text-2xl font-bold mb-4 text-center">
+              Feedback Form
+            </h2>
+            <form className="space-y-4" onSubmit={handleSubmit}>
+              <div>
+                <label
+                  htmlFor="name"
+                  className="block text-sm font-medium text-gray-700 mb-1"
+                >
+                  Name
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  value={name}
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                  name="name"
+                  className="w-full px-3 py-2 border border-gray-300  focus:outline-none focus:ring focus:border-blue-300"
+                  placeholder="Enter your name"
+                  required
+                />
+              </div>
 
-            <div>
-              <label
-                htmlFor="message"
-                className="block text-sm font-medium text-gray-700 mb-1"
-              >
-                Message
-              </label>
-              <textarea
-                id="message"
-                name="message"
-                value={message}
-                onChange={(e) => {
-                  setMessage(e.target.value);
-                }}
-                className="w-full px-3 py-2 border border-gray-300  focus:outline-none focus:ring focus:border-blue-300"
-                placeholder="Enter your message (25-50 words)"
-                rows="4"
-                required
-              ></textarea>
-            </div>
+              <div>
+                <label
+                  htmlFor="message"
+                  className="block text-sm font-medium text-gray-700 mb-1"
+                >
+                  Message
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  value={message}
+                  onChange={(e) => {
+                    setMessage(e.target.value);
+                  }}
+                  className="w-full px-3 py-2 border border-gray-300  focus:outline-none focus:ring focus:border-blue-300"
+                  placeholder="Let us know your thoughts (25-50 words)"
+                  rows="4"
+                  required
+                ></textarea>
+              </div>
 
-            <button
-              type="submit"
-              className="w-full bg-blue-500 text-white py-2 px-4  hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300"
-            >
-              Submit
-            </button>
-          </form>
+              <button
+                type="submit"
+                className="w-full bg-blue-500 text-white py-2 px-4  hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300"
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+        <div>
+          <img
+            src="./feedback.jpg"
+            alt="feedbackImg"
+            className="h-[500px] w-[400px] pt-5"
+          />
         </div>
       </div>
-      <div className="border-t border-gray-300 gap-10 py-10 flex flex-wrap mt-10">
+
+      <div className="border-t border-gray-300 gap-10 py-10 flex flex-wrap mt-12">
         {feedback.map((feed) => {
           return (
             <FeedbackCard
